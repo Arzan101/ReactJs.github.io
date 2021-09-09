0fdaacd280fc633a7c886808ce7112ab55788dbe
Arzan101 <khanazzu994@gmail.com>
2021-09-09 12:52:34 +0530

    imp

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css'
+import add from "./List";
 
-let greetings=''
-const cssStyle={};
-
-let curDate = new Date(2021,9,5,15)
-curDate= curDate.getHours()
-if(curDate>=1 && curDate <12 ){
-  greetings = "Good Morning"
-  cssStyle.color= 'green';
-}
-else if(curDate>12 && curDate <20)
-{
- greetings = "Good Afternoon" 
- cssStyle.color= 'orange';
-}
-else{
-  greetings="Good Night"
-  cssStyle.color= 'black';
-}
 
 ReactDOM.render(
   <React.Fragment>
-  <div>
-  <h1>Hello sir , <span style={cssStyle}>{greetings}</span></h1>
-  </div>
+  
+  <h1> "Enter both Number" {add(40,15)} </h1>
   </React.Fragment>,
   document.getElementById("root")
+
 );
