ea37598b2cef4287c4411f4427f78de7e259aab4
Arzan101 <khanazzu994@gmail.com>
2021-11-24 22:45:16 +0530

    master
    master

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css"
-// import "./SlotM"
- import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+  ReactDOM.render(
+  <App />,
+    document.getElementById("root")
+  );
 
 
-ReactDOM.render(
-  <React.Fragment>
-  <div>
-   <App/>
-  </div>
-  </React.Fragment>,
-  document.getElementById("root")
 
-);
+
